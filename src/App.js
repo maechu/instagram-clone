a02bce26670e17b511720e241bb754a46f382369
keem-hyun <hw813@naver.com>
2022-08-17 02:41:48 +0900

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,28 +3,34 @@ import Navbar from './components/Navbar';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
+import { Outlet, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 // 아이콘 라이브러리
 library.add(fas, far);
 
+const Layout = () => {
+  return(
+    <div>
+      <Navbar />
+
+      <Outlet />
+    </div>
+  )
+}
 
 function App() {
   return (
-    <Navbar />
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<MainPage />}/>
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
 export default App;
 
-// export default App;
-// function App() {
-//   return (<BrowserRouter>
-//       <Routes>
-//         <Route path="/main-page" element={<MainPage />} />
-//       </Routes>
-//   </BrowserRouter>
-//   );
-// }
-
-// export default App;
 
